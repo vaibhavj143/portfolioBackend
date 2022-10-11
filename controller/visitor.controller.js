370abfe5326d423605e9f15941272525a411ae65
vaibhav joshi <vaibhavj143.vj.vj@gmail.com>
2022-10-11 15:55:27 +0530

    insert /controller

--- controller/visitor.controller.js
+++ controller/visitor.controller.js
@@ -5,7 +5,7 @@ import Visitor from "../models/visitor.model.js";
 export const addVisitor = async (req, res) => {
   try {
     const body = req.body;
-    const visit = await Visitor.insertOne({...body});
+    const visit = await Visitor.insert({...body});
 
     const transporter = await transporterFunction();
 
