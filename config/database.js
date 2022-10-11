dbd56cfd5e698944326dfa2203d6500555c0131d
vaibhav joshi <vaibhavj143.vj.vj@gmail.com>
2022-10-11 17:27:37 +0530

    save /controller

--- config/database.js
+++ config/database.js
@@ -7,17 +7,13 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const dataBase = async() =>{
-
-    try {
-        
-        await  mongoose.connect(process.env.DATABASE)
-
-    } catch (error) {
-     
-        console.log(`${error.message}`);
-        
-    }
+const dataBase = () =>{
+    
+          mongoose.connect(process.env.DATABASE,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true   }).then(() => console.log("Database connected!"))
+            .catch(err => console.log(err));
+  
 }
 
-export default dataBase
+export default dataBase ;
